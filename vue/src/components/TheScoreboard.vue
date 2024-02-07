5a4ee42c83d0bfa5f00015c3d0473b132e8efdc2
delusional51 <BenjaminZanelli51@gmail.com>
2024-02-07 13:12:23 -0500

    idk man

---
+++ vue/src/components/TheScoreboard.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>{{ test.name }}</h1>
+  </template>
+
+<script setup>
+const props = defineProps({
+  tests:Object,
+})
+</script>
+  
+<style scoped>
+
+</style>
---
+++ vue/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import TheScoreboard from '@/components/TheScoreboard.vue';
+const test = [
+  {name:"hi"}
+]
+</script>
+
+<template>
+  <main>
+    <TheScoreboard
+    v-for="thing in test"
+    :key="thing.name"
+    :tests="thing"
+    ></TheScoreboard>
+  </main>
+</template>
