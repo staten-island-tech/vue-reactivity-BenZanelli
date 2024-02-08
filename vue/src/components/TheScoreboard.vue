ef7703011da1c098ad81e1a62b52fe56bfc5b15d
Ben Zanelli <BenjaminZanelli51@gmail.com>
2024-02-08 12:33:12 -0500

    im so good

--- vue/src/components/TheScoreboard.vue
+++ vue/src/components/TheScoreboard.vue
@@ -1,13 +1,18 @@
 <template>
-    <h1>{{ test.name }}</h1>
-  </template>
+  <header>
+    <div class= "h-20 flex justify-center bg-slate-600 w-1/2 m-auto text-center mt-3">
+     <h1 class="flex justify-center mt-2.5">Scoreboard</h1>
+     <div class=""></div>
+</div>
+  </header>
+</template>
 
 <script setup>
-const props = defineProps({
-  tests:Object,
-})
+
+
+
 </script>
-  
+
 <style scoped>
 
 </style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,16 +1,14 @@
+<template>
+  <div>
+    <TheScoreboard/>
+  </div>
+</template>
+
 <script setup>
 import TheScoreboard from '@/components/TheScoreboard.vue';
-const test = [
-  {name:"hi"}
-]
+
 </script>
 
-<template>
-  <main>
-    <TheScoreboard
-    v-for="thing in test"
-    :key="thing.name"
-    :tests="thing"
-    ></TheScoreboard>
-  </main>
-</template>
+<style scoped>
+
+</style>
