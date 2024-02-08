3fcae42f5bece8621adbd9f487b870de3c69ce3e
delusional51 <BenjaminZanelli51@gmail.com>
2024-02-08 13:13:17 -0500

    I LOVE TAILWIND

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -3,13 +3,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-
-    <div class="wrapper">
-
-      
-    </div>
-  </header>
+  
 
   <RouterView />
 </template>
