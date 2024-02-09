aa4c23bc9f2c84f06bb3db906013532ae0afc75f
Ben Zanelli <BenjaminZanelli51@gmail.com>
2024-02-09 16:11:17 -0500

    asdlkfhasd;lkjghaewlkugbansdliufgbewqiughadsliufgbqwefiuh

--- vue/src/components/WeaponChoose.vue
+++ vue/src/components/WeaponChoose.vue
@@ -1,38 +1,38 @@
 <template>
-   
-        <h1 class=" text-pretty text-4xl underline font-semibold">Choose Your Weapon</h1>
-        <div class="choices">
-            <form action="" class="flex items-center justify-center flex-col" id="form" @submit.prevent>
-                <div class="ep flex flex-row mb-2 mt-2">
-                <input type="radio" id="epee" name="weapon" value="epee" v-on="chosen" >
-                <label for="epee">EPEE</label><br>
-                </div>
-                <div class="fl flex flex-row mb-2">
-                <input type="radio" id="foil" name="weapon" value="foil">
-                <label for="foil">FOIL</label><br>
-                </div>
-                <div class="sbr flex flex-row mb-2">
-                     <input type="radio" id="sabre" name="weapon" value="sabre" >
-                    <label for="sabre">SABRE</label><br>
-                    
-            </div>
-            </form> 
-            
-        </div>
-    </template>
-    
-    <script setup>
-    import {ref} from  'vue'
-    import { RouterLink, RouterView } from 'vue-router';
-    const chosen = ref("")
-    const form= document.querySelector("#form")
-    const props = defineProps({
-        gamestart:Boolean, 
-        chosen:String
-    })
+  <h1 class="text-pretty text-4xl underline font-semibold">
+    Choose Your Weapon
+  </h1>
+  <div class="choices">
+    <form
+      action=""
+      class="flex items-center justify-center flex-col"
+      id="form"
+      @submit.prevent
+    >
+      <div class="ep flex flex-row mb-2 mt-2">
+        <input type="radio" id="epee" name="weapon" value="epee" />
+        <label for="epee">EPEE</label><br />
+      </div>
+      <div class="fl flex flex-row mb-2">
+        <input type="radio" id="foil" name="weapon" value="foil" />
+        <label for="foil">FOIL</label><br />
+      </div>
+      <div class="sbr flex flex-row mb-2">
+        <input type="radio" id="sabre" name="weapon" value="sabre" />
+        <label for="sabre">SABRE</label><br />
+      </div>
+    </form>
+  </div>
+</template>
 
-
-    </script>
-    <style scoped>
-    
-    </style>
+<script setup>
+import { ref } from "vue";
+import { RouterLink, RouterView } from "vue-router";
+const form = document.querySelector("#form");
+const props = defineProps({
+  gamestart: Boolean,
+  chosen: String,
+});
+const chosen = ref("");
+</script>
+<style scoped></style>
--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,47 +1,64 @@
 <template>
-    <div>
-      <header class="grid grid-cols-4" v-if="!gamestart & !moveset" >
-        <TheScoreboard class=" col-start-2 col-span-2 w-full"/>
-        <h2 @click="moves" class="text-white h-12 shadow-2xl shadow-stone-400 rounded-lg hover:text-blue-400 flex w-32 underline m-auto items-center justify-center text-xl border-2 border-slate-700 bg-slate-600">Move Sets</h2>
-  
-      </header>
-      <div class="flex items-center justify-center flex-col m-auto mt-36 h-72 w-2/3 bg-red-400 aspect-square border-8 border-black rounded-3xl"  v-if="gamestart">
-      <WeaponChoose 
-      
-      />
-      <button type="submit" value="PLAY GAME!" class="bg-white border-2 border-slate-300 w-52 hover:bg-slate-200 font-semibold" @click="toggle">Play Game!</button>
+  <div>
+    <header class="grid grid-cols-4" v-if="!gamestart & !moveset">
+      <TheScoreboard class="col-start-2 col-span-2 w-full" />
+      <h2
+        @click="moves"
+        class="text-white h-12 shadow-2xl shadow-stone-400 rounded-lg hover:text-blue-400 flex w-32 underline m-auto items-center justify-center text-xl border-2 border-slate-700 bg-slate-600"
+      >
+        Move Sets
+      </h2>
+    </header>
+    <div
+      class="flex items-center justify-center flex-col m-auto mt-36 h-72 w-2/3 bg-red-400 aspect-square border-8 border-black rounded-3xl"
+      v-if="gamestart"
+    >
+      <WeaponChoose />
+      <button
+        type="submit"
+        value="PLAY GAME!"
+        class="bg-white border-2 border-slate-300 w-52 hover:bg-slate-200 font-semibold"
+        @click="toggle"
+      >
+        Play Game!
+      </button>
     </div>
-     <button v-if="!gamestart & moveset" @click="moves">Go back</button>
-    </div>
-    <RouterView/>
-  </template>
+    <button v-if="!gamestart & moveset" @click="moves">Go back</button>
+  </div>
+  <RouterView />
+</template>
 
-  <script setup>
-  import TheScoreboard from '@/components/TheScoreboard.vue';
-  import WeaponChoose from '@/components/WeaponChoose.vue';
-  import { RouterView, RouterLink } from 'vue-router';
-  import { ref } from 'vue';
-  const gamestart = ref(true)
-  const moveset = ref(false)
-  function toggle(){
-    gamestart.value= !gamestart.value
-  }
-  function moves(){
-    moveset.value= !moveset.value
-  }
-  const weapons = [
-    {
-      name:"epee"
-    },
-    {
-      name:"foil"
-    },
-    {
-      name:"sabre"
-    }
-  ]
-  </script>
+<script setup>
+import TheScoreboard from "@/components/TheScoreboard.vue";
+import WeaponChoose from "@/components/WeaponChoose.vue";
+import { RouterView, RouterLink } from "vue-router";
+import { ref } from "vue";
+// import { chosen } from "../components/WeaponChoose.vue";
+const gamestart = ref(true);
+const moveset = ref(false);
+function getWeapon() {
+  // console.log(chosen.value);
+  // weapon = chosen.value;
+  // return weapon;
+}
+function toggle() {
+  gamestart.value = !gamestart.value;
+  getWeapon();
+}
+function moves() {
+  moveset.value = !moveset.value;
+}
+const weapons = [
+  {
+    name: "epee",
+  },
+  {
+    name: "foil",
+  },
+  {
+    name: "sabre",
+  },
+];
+</script>
 
-  <style scoped>
-  
-  </style>
+<style scoped></style>
